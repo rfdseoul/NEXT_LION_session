a27d57d5329bcc609dfb0e1619dfe3c7bfe5586b
jeonmincheol <89579178+rfdseoul@users.noreply.github.com>
2022-07-11 01:07:23 +0900

    로그인 기능 추가

---
+++ prac1/project/post/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from post import views
+
+app_name = 'post'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new/', views.new, name='new'),
+    path('detail/<int:post_pk>', views.detail, name='detail'),
+    path('edit/<int:post_pk>', views.edit, name='edit'),
+    path('delete/<int:post_pk>', views.delete, name='delete'),
+    path('post/signup', views.signup, name='signup'),
+    path('post/login', views.login, name='login'),
+    path('post/logout', views.logout, name='logout'),
+    ]
--- prac1/project/post/views.py
+++ prac1/project/post/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {
+    return render(request, 'post/home.html', {
         'posts':posts,
     })
 
@@ -14,12 +16,12 @@ def new(request):
             title = request.POST['title'],
             content = request.POST['content'],
         )
-        return redirect('home')
-    return render(request, 'new.html')
+        return redirect('post:home')
+    return render(request, 'post/new.html')
 
 def detail(request, post_pk):
     post = Post.objects.get(pk=post_pk)
-    return render(request, 'detail.html', {
+    return render(request, 'post/detail.html', {
         'post':post,
     })
 
@@ -30,8 +32,8 @@ def edit(request, post_pk):
             title = request.POST['title'],
             content = request.POST['content'],
         )
-        return redirect('detail', post_pk)
-    return render(request, 'edit.html', {
+        return redirect('post:detail', post_pk)
+    return render(request, 'post/edit.html', {
         'post':post,
     })
 
@@ -39,4 +41,39 @@ def delete(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     post.delete()
 
-    return redirect('home')
+    return redirect('post:home')
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        found_user = User.objects.filter(username=username)
+
+        if len(found_user):
+            error = "아이디가 이미 존재합니다."
+            return render(request, 'post/signup.html', {
+                'error':error,
+            })
+        new_user = User.objects.create_user(username=username, password=password)
+        auth.login(request, new_user)
+        return redirect('post:home')
+    return render(request, 'post/signup.html')
+
+def login(request):
+    if request.method == "post":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        error = "아이디 또는 비밀번호가 틀립니다."
+        return render(request, 'post/login.html', {
+            'error':error,
+        })
+    return render(request, 'post/login.html')
+
+def logout(request):
+    auth.logout(request)
+
+    return redirect('post:home')
