b6f66c285ce908292e79639e8038e9410b7fe050
jeonmincheol <89579178+rfdseoul@users.noreply.github.com>
2022-07-12 15:51:52 +0900

    각각 다른 app에 설정한 static 파일 base.html에 적용

--- prac1/project/post/urls.py
+++ prac1/project/post/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('detail/<int:post_pk>', views.detail, name='detail'),
     path('edit/<int:post_pk>', views.edit, name='edit'),
     path('delete/<int:post_pk>', views.delete, name='delete'),
-    path('post/signup', views.signup, name='signup'),
-    path('post/login', views.login, name='login'),
-    path('post/logout', views.logout, name='logout'),
+    # path('post/signup', views.signup, name='signup'),
+    # path('post/login', views.login, name='login'),
+    # path('post/logout', views.logout, name='logout'),
     ]
--- prac1/project/post/views.py
+++ prac1/project/post/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
@@ -10,6 +11,7 @@ def home(request):
         'posts':posts,
     })
 
+@login_required
 def new(request):
     if request.method == "POST":
         new_post = Post.objects.create(
@@ -25,6 +27,7 @@ def detail(request, post_pk):
         'post':post,
     })
 
+@login_required
 def edit(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     if request.method == "POST":
@@ -37,43 +40,10 @@ def edit(request, post_pk):
         'post':post,
     })
 
+@login_required
 def delete(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     post.delete()
 
     return redirect('post:home')
 
-def signup(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        found_user = User.objects.filter(username=username)
-
-        if len(found_user):
-            error = "아이디가 이미 존재합니다."
-            return render(request, 'post/signup.html', {
-                'error':error,
-            })
-        new_user = User.objects.create_user(username=username, password=password)
-        auth.login(request, new_user)
-        return redirect('post:home')
-    return render(request, 'post/signup.html')
-
-def login(request):
-    if request.method == "post":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('home')
-        error = "아이디 또는 비밀번호가 틀립니다."
-        return render(request, 'post/login.html', {
-            'error':error,
-        })
-    return render(request, 'post/login.html')
-
-def logout(request):
-    auth.logout(request)
-
-    return redirect('post:home')
