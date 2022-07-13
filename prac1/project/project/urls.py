866fd14a894bfab7ba18c6b33cf874296fedb67c
jeonmincheol <89579178+rfdseoul@users.noreply.github.com>
2022-07-13 14:57:12 +0900

    메인 페이지(8000/) url  post에 있는 views.home으로 추가

--- prac1/project/project/urls.py
+++ prac1/project/project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from post import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     path('post/', include('post.urls')),
     path('accounts/', include('accounts.urls')),
     ]
